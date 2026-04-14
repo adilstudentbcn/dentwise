df956b8473205e83c37c765fa415bab06b06a2b4
adilstudentbcn <adil.student.bcn@gmail.com>
2026-04-14 04:40:29 +0200

    sync user clerk neon

---
+++ src/lib/prisma.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const prismaClientSingleton = () => {
+  // 1. Create a connection pool using your DATABASE_URL
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  
+  // 2. Create the adapter
+  const adapter = new PrismaPg(pool);
+  
+  // 3. Pass the adapter to the constructor
+  return new PrismaClient({ adapter });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
